9ef1b18729587b1dac02cdb768887fd43a065b40
2138422c <2138422C@student.gla.ac.uk>
2018-02-14 17:18:56 +0000

    Added in while loop that will be used when storing

--- why.c
+++ why.c
@@ -2,10 +2,17 @@
 
 int main(int argc, char **argv)
 {
+	int i = 0;
 	// TODO: decide on options
 	if(argc < 2) {
 		printf("usage: why [options] [program] [...]\n");
 		return -1;
+	}
+	
+	while(i < argc) {
+		// TODO: add each program to data structure to store them in this loop
+		printf("%s\n", *(argv+i));
+		i++;
 	}
 
 	printf("I don't know why you installed this yet\n");
