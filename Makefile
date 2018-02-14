0cbb838c6babac9680050e0d804a405655c8682c
2138422c <2138422C@student.gla.ac.uk>
2018-02-14 16:44:30 +0000

    Checks for number of arguments, made Makefile and updated .gitignore to include executable why

---
+++ Makefile
@@ -0,0 +1,2 @@
+all: why.c
+	gcc -Wall why.c -o why
--- why.c
+++ why.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char **argv)
 {
+	// TODO: decide on options
+	if(argc < 2) {
+		printf("usage: why [options] [program] [...]\n");
+		return -1;
+	}
+
 	printf("I don't know why you installed this yet\n");
 	return 0;
 }
